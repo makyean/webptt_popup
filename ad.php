8702c12ebe5b90c77b53295ba57d467b8c9a0ed6
makyean <mak.yang@adgeek.com.tw>
2016-11-10 11:25:09 +0800

    Create ad.php

---
+++ ad.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="stylesheets/popup.css?t=<?php echo rand(); ?>" type="text/css" />
+
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<title>WebPtt_IdlePage</title>
+	</head>
+	<body>
+		<div class="container" style="background-color:#FFF;font-family:Microsoft JhengHei">
+			<center>
+				<div class="line" style="width:728px;"></div>
+			</center>
+			<div class="top_ad_zoom">
+				<div id="webptt_top_72890">
+					<center>
+						<!-- BEGIN JS TAG - TW/Webptt_728x90_ROS_3 < - DO NOT MODIFY -->
+						<SCRIPT SRC="//ib.adnxs.com/ttj?id=9332705&cb=[CACHEBUSTER]&pubclick=[INSERT_CLICK_TAG]" TYPE="text/javascript"></SCRIPT>
+						<!-- END TAG -->
+					</center>
+				</div>
+			</div>
+			<center>
+				<div class="line" style="width:728px;"></div>
+			</center>
+			<div style="width:728px; margin:0 auto;">
+				<center>
+					<div class="bottom_ad_zoom">
+						<div class="bottom_right" style="width:300px;float:left;">
+							<div id="webptt_bottom_right_300250" style="margin-top:10px;">
+								<!-- BEGIN JS TAG - TW/Webptt_300x250_ROS < - DO NOT MODIFY -->
+								<SCRIPT SRC="//ib.adnxs.com/ttj?id=9332706&cb=[CACHEBUSTER]&pubclick=[INSERT_CLICK_TAG]" TYPE="text/javascript"></SCRIPT>
+								<!-- END TAG -->
+							</div>
+						</div>
+						<div class="bottom_left" style="width:428px;float:right;">
+							<div id="bottom_left_title">
+								<h1 style="color:black;font-size:24px;"> 即 時 熱 門 看 板 </h1>
+							</div>
+							<div id="bottom_left_hotkey" style="width:400px;">
+								<table class="table table-hover" style="text-align: center;font-size:18px;">
+									<tbody>
+										<tr>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/Gossiping/index.html" target="_blank"> 八 卦 版 </a></td>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/Tech_Job/index.html" target="_blank"> 科 技 版 </a></td>
+										</tr>
+										<tr>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/LoL/index.html" target="_blank"> LOL 版 </a></td>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/NBA/index.html" target="_blank"> NBA 版 </a></td>
+										</tr>
+										<tr>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/ToS/index.html" target="_blank"> 神 魔 版 </a></td>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/Baseball/index.html" target="_blank"> 棒 球 版 </a></td>
+										</tr>
+										<tr>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/movie/index.html" target="_blank"> 電 影 版 </a></td>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/WomenTalk/index.html" target="_blank"> 女 孩 版 </a></td>
+										</tr>
+										<tr>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/joke/index.html" target="_blank"> 就 可 版 </a></td>
+											<td><a href="https://webptt.com/m.aspx?n=bbs/C_Chat/index.html" target="_blank"> 希 洽 版 </a></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</center>
+			</div>
+			<center>
+				<div class="line" style="width:728px;"></div>
+			</center>
+		</div>
+	</body>
+</html>
